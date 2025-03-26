a832868b0b987359e3dc8dfe161a3641b0ab5fd2
DylanFoster89194 <102839402+DylanFoster89194@users.noreply.github.com>
2025-03-26 13:22:28 -0400

    dynamic die activity

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.res.Configuration
+
 
 
 /*
@@ -25,18 +27,33 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
 
         /* TODO 1: Load fragment(s)
             - Show _only_ ButtonFragment if portrait
-            - show _both_ fragments if Landscape
-          */
-    }
-
-    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
+            - Show _both_ fragments if Landscape
         */
 
-    // This callback function gets invoked when the child Fragment invokes it
-    // Remember to place Fragment transactions on BackStack so then can be reversed
-    override fun buttonClicked() {
-
+        if (savedInstanceState == null) {
+            if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                // Portrait mode: Show only ButtonFragment
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, ButtonFragment())
+                    .commit()
+            } else {
+                // Landscape mode: Show both ButtonFragment and DieFragment
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.buttonFragmentContainer, ButtonFragment())
+                    .replace(R.id.dieFragmentContainer, DieFragment())
+                    .commit()
+            }
+        }
     }
 
-
+    /* TODO 2: Switch fragments if die rolled and in portrait (no need to switch fragments if Landscape) */
+    override fun buttonClicked() {
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Replace ButtonFragment with DieFragment when button is clicked
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, DieFragment())
+                .addToBackStack(null) // Allows user to go back
+                .commit()
+        }
+    }
 }
